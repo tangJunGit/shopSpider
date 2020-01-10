168b34893d1b9a172f902474ba7a00bdcb8d6805
tangjun <1031500697@qq.com>
2020-01-10 10:31:15 +0800

    获取导航条链接

--- shopSpider/spiders/zcn.py
+++ shopSpider/spiders/zcn.py
@@ -4,8 +4,8 @@ import scrapy
 
 class ZcnSpider(scrapy.Spider):
     name = 'zcn'
-    allowed_domains = ['http://hhanbag.com/']
-    start_urls = ['http://hhanbag.com/']
+    allowed_domains = ['http://www.hhanbag.com']
+    start_urls = ['http://www.hhanbag.com']
 
     config = {}          # settings自定义配置项
     filter_main_navBars = []    #  过滤后的一级菜单
@@ -20,15 +20,34 @@ class ZcnSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        self.getMainCategory(response)
-        
+        # 获取一级菜单
+        self.getMainNavBars(response)
 
-    # 获取一级菜单主分类
-    def getMainCategory(self, response):
+        # 获取分类链接
+        self.getCategoryUrls()
+
+        # 请求商品列表
+        for url in self.category_urls:
+            yield scrapy.Request(url=url, callback=self.handleProductList, dont_filter=True)
+            
+
+    # 获取一级菜单
+    def getMainNavBars(self, response):
         main_navBars = response.css(self.config['mainNavBarSelector'])
         for index,navBar in enumerate(main_navBars):
             # 过滤一级菜单导航，去掉不需要的
             if index  not in self.config['mainNavBarFilterByIndex']:
                 self.filter_main_navBars.append(navBar)
-        
-        print(self.category_urls)
+
+    
+    # 获取分类链接
+    def getCategoryUrls(self):
+        for navBar in self.filter_main_navBars:
+            hrefs = navBar.css('a::attr(href)').extract()
+            for href in hrefs:
+                self.category_urls.append(href)
+                
+
+    # 处理商品列表
+    def handleProductList(self, response):
+        print(response)
